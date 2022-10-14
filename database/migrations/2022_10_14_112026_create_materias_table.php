03203e691e507ec9de95519ae81496f6822e99d4
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2022-10-14 09:26:40 -0300

    14/10/22

--- database/migrations/2022_10_14_112026_create_materias_table.php
+++ database/migrations/2022_10_14_112026_create_materias_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('Nome');
             $table->string('Creditos');
-            $table->string('Professor');
+            $table->string('RP');
             $table->timestamps();
         });
     }
